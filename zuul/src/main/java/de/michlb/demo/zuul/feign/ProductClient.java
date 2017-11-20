7ac8309461ed53e5d0398c9cd076192967525d59
성일 강 <1700429@partner.skcc.com>
2017-11-20 16:22:25 +0900

    버전 정보 수정

--- zuul/src/main/java/de/michlb/demo/zuul/feign/ProductClient.java
+++ zuul/src/main/java/de/michlb/demo/zuul/feign/ProductClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient("product")
+@FeignClient("product-v1")
 public interface ProductClient {
 
   @RequestMapping(value = "api/product/detail", method = RequestMethod.GET)
